9d0be037b2a1378b7dee9e42267ecac476b78964
andriykhomik <andriykhomik1992@gmail.com>
2021-12-09 19:34:51 +0200

    finished this 50 Projects

--- main.js
+++ main.js
@@ -4,17 +4,19 @@ const startBtn = document.querySelector('#start-btn');
 const scoreEl = document.querySelector('#score');
 const timeEl = document.querySelector('#time');
 const messageEl = document.querySelector('#message');
-
+const gameContainer = document.querySelector('.game-container');
 let screen = 0;
-let chosenInsect;
+let chosenInsect = '';
 let score = 0;
 let seconds = 0;
 let minutes = 0;
+
 
 startBtn.addEventListener('click', ()=> {
     screens[screen].classList.add('up');
     screen++;
 })
+
 
 insectButtons.forEach(insectBtn => {
     insectBtn.addEventListener('click', (e)=> {
@@ -31,6 +33,7 @@ const start = ()=> {
     setInterval(createInsect, 2000);
 }
 
+
 function createInsect(){
 
     const insectEl = document.createElement('div');
@@ -38,31 +41,32 @@ function createInsect(){
 
     insectEl.style.left = randomizer(window.innerWidth) + 'px';
     insectEl.style.top = randomizer(window.innerHeight) + 'px';
-    insectEl.style.transform = `rotate(${randomizer(360)}deg)`;
 
-    insectEl.addEventListener('click', () => {
+    const src =  chosenInsect.getAttribute('src');
+    insectEl.innerHTML = `<img src="${src}" alt="" style="transform: rotate(${randomizer(360)}deg)"/>`;
 
-        insectEl.classList.add('caught');
-
-        hide();
-        createInsect();
-    })
-        const imgEl = document.createElement('img');
-        imgEl.src = chosenInsect.src;
-
-    insectEl.appendChild(imgEl);
-    screens[screen].appendChild(insectEl);
+    insectEl.addEventListener('click', hide);
+    gameContainer.appendChild(insectEl);
 }
 
+
 function hide(){
-    console.log(this)
-    // insect.remove();
+
+    this.classList.add('caught');
+    setTime(this.remove, 2000);
+
     score++;
+
     scoreEl.innerText = `Score: ${score}`;
+
     if (score > 19){
         messageEl.classList.add('visible');
     }
+
+    const insects = document.querySelectorAll('.insect');
+    console.log (insects);
 }
+
 
 function randomizer(value){
     if (value !== 360){
@@ -72,9 +76,6 @@ function randomizer(value){
     }
 
 }
-
-
-
 
 
 function setTime(){
